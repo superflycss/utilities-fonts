dd95aa8bc0fe245538d748a09f237049e40fa9b0
Ole Ersoy <ole.ersoy@gmail.com>
2017-07-12 13:17:44 -0500

    Added tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
 require('@superflycss/task-clean');
 require('@superflycss/task-build');
+require('@superflycss/task-nunjucks');
 require('@superflycss/task-test');
 require('@superflycss/task-deploy');
 
-var PLI = require('@superflycss/pli');
-var gulp = require('gulp');
-
-gulp.task('watch', function() {
-    gulp.watch(PLI.SRC_TEST_HTML, ['clean', 'build:css', 'test:css'])
-});
