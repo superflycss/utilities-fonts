59b2a4a5e3bf3fe3f19f2108a983ca6572d3c912
Ole <ole.ersoy@gmail.com>
2018-05-02 19:32:09 -0500

    Added generation scripts for fonts

--- gulpfile.js
+++
@@ -1,6 +0,0 @@
-require('@superflycss/task-clean');
-require('@superflycss/task-build');
-require('@superflycss/task-nunjucks');
-require('@superflycss/task-test');
-require('@superflycss/task-deploy');
-
