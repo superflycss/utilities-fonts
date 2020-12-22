d996d45bdd2b1be1d54ae09e8c1402a8cc4b0944
Ole Ersoy <ole.ersoy@gmail.com>
2020-12-21 20:22:20 -0600

    New version of library

--- README.md
+++ README.md
@@ -1,41 +1,66 @@
 # Font utilities
 
-[![Greenkeeper badge](https://badges.greenkeeper.io/superflycss/utilities-fonts.svg)](https://greenkeeper.io/)
-
 [![Build Status](https://travis-ci.org/superflycss/utilities-fonts.svg?branch=master)](https://travis-ci.org/superflycss/utilities-fonts)
 
 ## Installation
 
-See [Installation](https://github.com/superflycss/superflycss/#installation).
+```
+npm i @superflycss/utilities-fonts
+```
 
 ## Usage
 
 General [usage instructions](https://github.com/superflycss/superflycss/#usage).  
 
-The Google fonts are located in individual sub packages.  For example '@superflycss/utilities-fonts/lato'.  The reason for this is
-that browser performance is sensitive to the number of custom fonts loaded.  For example when all Google fonts are loaded for all possible font weight values the page usually crashes or becomes completely unresposive.
+The Google fonts are located in individual sub packages.  For example '@superflycss/utilities-fonts/lato'.  
 
-To load the top level module and the `lato`  font first install `npm i -S @superflycss/utilities-fonts` and then:
 
+To import it using the following CSS declaration:
 ``` css
-@import '@superflycss/utilities-fonts';
 @import '@superflycss/utilities-fonts/lato';
-
 ...
 
 ```
 
+To use the `lato` font at normal weight apply the utility
+like this:
+
 ``` html
-<h1 class="u-font-lato">A Font Lato Heading!</h1>
-<h1 class="u-font-lato-fw800">A Bold Font Lato Heading!</h1>
-<h1 class="u-font-lato-fs800-fw800">A Big and Bold Font Lato Heading!</h1>
+<h1 class="u-font-lato-400">A Font Lato Heading Normal Font Weight (400)</h1>
 ```
 
-Now run the build with `sfc build` and `postcss font-magician` will have created font face rules for the `lato` font across the range of [SuperflyCSS Font Variable](https://github.com/superflycss/variables-fonts)  sizes and weights.
+To use the italic `lato` font at normal weight apply the utility like this:
 
-## Distribution
+``` html
+<h1 class="u-font-lato-400-italic">A Font Lato Heading Normal Font Weight (400)</h1>
+```
 
-A custom script `src/main/js/index.js` is used to generate the Google Font utility subpackages.  Running the script creates a subpackage (Directory) for each google font in `./dist`.  Note that the subpackages do not generate into `src/main/css` in order to keep this directory clean and workable, and to avoid committing the 800+ generated modules to the github repository.
+
+Now run the build with `sfc build` and `postcss font-magician` will create the font face rules in the target build file.
+
+## Dependency Install
+
+This will install all dependencies
+including the SuperflyCSS CLI.
+
+```
+npm run i
+```
+
+## Build
+
+```
+npm run pub
+```
+
+## Publish
+
+```
+npm run pub
+```
+
+
+Note that the subpackages do not generate into `src/main/css` in order to keep this directory clean and workable, and to avoid committing the 800+ generated modules to the github repository.
 
 ```
 git clone git@github.com:superflycss/utilities-fonts.git
@@ -44,11 +69,6 @@ sfc dist
 cd dist
 npm publish
 ```
-
-
-## Tests
-
-[Superfly CSS Utilities Fonts Tests](https://superflycss.github.io/utilities-fonts/target/test/html/)
 
 ## Related Articles
 
--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,111 +1,103 @@
-const mkdirp = require('mkdirp');
-const PLI = require('@superflycss/pli');
-const fs = require('fs');
-const path = require('path');
-const fontmap = require('./fontmap');
+var fs = require("fs");
+var path = require("path");
+var mkdirp = require("mkdirp");
+const PLI = require("@superflycss/pli");
+const del = require("del");
 
-const sizes = [100, 200, 300, 400, 500, 600, 700, 800, 900, 'xs', 'sm', 'lg'];
-
-const sizeMap = {
-    100: '0.750rem',
-    200: '0.875rem',
-    300: '1.000rem',
-    400: '1.250rem',
-    500: '1.500rem',
-    600: '1.750rem',
-    700: '2.000rem',
-    800: '2.500rem',
-    900: '3.000rem',
-
-    xs: '0.750rem',
-    sm: '0.875rem',
-    lg: '1.250rem'
-}
-
-
-const weights = [100, 200, 300, 400, 500, 600, 700, 800, 900];
-
-const fonts = ['abeezee', 'abel', 'abhaya-libre', 'abril-fatface', 'aclonica', 'acme', 'actor', 'adamina', 'advent-pro', 'aguafina-script', 'akronim', 'aladin', 'aldrich', 'alef', 'alegreya', 'alegreya-sc', 'alegreya-sans', 'alegreya-sans-sc', 'alex-brush',
-    'alfa-slab-one', 'alice', 'alike', 'alike-angular', 'allan', 'allerta', 'allerta-stencil', 'allura', 'almendra', 'almendra-display', 'almendra-sc', 'amarante', 'amaranth', 'amatic-sc', 'amatica-sc', 'amethysta', 'amiko', 'amiri', 'amita', 'anaheim',
-    'andada', 'andika', 'angkor', 'annie-use-your-telescope', 'anonymous-pro', 'antic', 'antic-didone', 'antic-slab', 'anton', 'arapey', 'arbutus', 'arbutus-slab', 'architects-daughter', 'archivo-black', 'archivo-narrow', 'aref-ruqaa', 'arima-madurai',
-    'arimo', 'arizonia', 'armata', 'arsenal', 'artifika', 'arvo', 'arya', 'asap', 'asar', 'asset', 'assistant', 'astloch', 'asul', 'athiti', 'atma', 'atomic-age', 'aubrey', 'audiowide', 'autour-one', 'average', 'average-sans', 'averia-gruesa-libre', 'averia-libre',
-    'averia-sans-libre', 'averia-serif-libre', 'bad-script', 'bahiana', 'baloo', 'baloo-bhai', 'baloo-bhaina', 'baloo-chettan', 'baloo-da', 'baloo-paaji', 'baloo-tamma', 'baloo-thambi', 'balthazar', 'bangers', 'barrio', 'basic', 'battambang', 'baumans',
-    'bayon', 'belgrano', 'belleza', 'benchnine', 'bentham', 'berkshire-swash', 'bevan', 'bigelow-rules', 'bigshot-one', 'bilbo', 'bilbo-swash-caps', 'biorhyme', 'biorhyme-expanded', 'biryani', 'bitter', 'black-ops-one', 'bokor', 'bonbon', 'boogaloo', 'bowlby-one',
-    'bowlby-one-sc', 'brawler', 'bree-serif', 'bubblegum-sans', 'bubbler-one', 'buda', 'buenard', 'bungee', 'bungee-hairline', 'bungee-inline', 'bungee-outline', 'bungee-shade', 'butcherman', 'butterfly-kids', 'cabin', 'cabin-condensed', 'cabin-sketch',
-    'caesar-dressing', 'cagliostro', 'cairo', 'calligraffitti', 'cambay', 'cambo', 'candal', 'cantarell', 'cantata-one', 'cantora-one', 'capriola', 'cardo', 'carme', 'carrois-gothic', 'carrois-gothic-sc', 'carter-one', 'catamaran', 'caudex', 'caveat', 'caveat-brush',
-    'cedarville-cursive', 'ceviche-one', 'changa', 'changa-one', 'chango', 'chathura', 'chau-philomene-one', 'chela-one', 'chelsea-market', 'chenla', 'cherry-cream-soda', 'cherry-swash', 'chewy', 'chicle', 'chivo', 'chonburi', 'cinzel', 'cinzel-decorative',
-    'clicker-script', 'coda', 'coda-caption', 'codystar', 'coiny', 'combo', 'comfortaa', 'coming-soon', 'concert-one', 'condiment', 'content', 'contrail-one', 'convergence', 'cookie', 'copse', 'corben', 'cormorant', 'cormorant-garamond', 'cormorant-infant',
-    'cormorant-sc', 'cormorant-unicase', 'cormorant-upright', 'courgette', 'cousine', 'coustard', 'covered-by-your-grace', 'crafty-girls', 'creepster', 'crete-round', 'crimson-text', 'croissant-one', 'crushed', 'cuprum', 'cutive', 'cutive-mono', 'damion',
-    'dancing-script', 'dangrek', 'david-libre', 'dawning-of-a-new-day', 'days-one', 'dekko', 'delius', 'delius-swash-caps', 'delius-unicase', 'della-respira', 'denk-one', 'devonshire', 'dhurjati', 'didact-gothic', 'diplomata', 'diplomata-sc', 'domine',
-    'donegal-one', 'doppio-one', 'dorsa', 'dosis', 'dr-sugiyama', 'droid-sans', 'droid-sans-mono', 'droid-serif', 'duru-sans', 'dynalight', 'eb-garamond', 'eagle-lake', 'eater', 'economica', 'eczar', 'ek-mukta', 'el-messiri', 'electrolize', 'elsie', 'elsie-swash-caps',
-    'emblema-one', 'emilys-candy', 'engagement', 'englebert', 'enriqueta', 'erica-one', 'esteban', 'euphoria-script', 'ewert', 'exo', 'exo-2', 'expletus-sans', 'fanwood-text', 'farsan', 'fascinate', 'fascinate-inline', 'faster-one', 'fasthand', 'fauna-one',
-    'federant', 'federo', 'felipa', 'fenix', 'finger-paint', 'fira-mono', 'fira-sans', 'fira-sans-condensed', 'fira-sans-extra-condensed', 'fjalla-one', 'fjord-one', 'flamenco', 'flavors', 'fondamento', 'fontdiner-swanky', 'forum', 'francois-one', 'frank-ruhl-libre',
-    'freckle-face', 'fredericka-the-great', 'fredoka-one', 'freehand', 'fresca', 'frijole', 'fruktur', 'fugaz-one', 'gfs-didot', 'gfs-neohellenic', 'gabriela', 'gafata', 'galada', 'galdeano', 'galindo', 'gentium-basic', 'gentium-book-basic', 'geo', 'geostar',
-    'geostar-fill', 'germania-one', 'gidugu', 'gilda-display', 'give-you-glory', 'glass-antiqua', 'glegoo', 'gloria-hallelujah', 'goblin-one', 'gochi-hand', 'gorditas', 'goudy-bookletter-1911', 'graduate', 'grand-hotel', 'gravitas-one', 'great-vibes', 'griffy',
-    'gruppo', 'gudea', 'gurajada', 'habibi', 'halant', 'hammersmith-one', 'hanalei', 'hanalei-fill', 'handlee', 'hanuman', 'happy-monkey', 'harmattan', 'headland-one', 'heebo', 'henny-penny', 'herr-von-muellerhoff', 'hind', 'hind-guntur', 'hind-madurai',
-    'hind-siliguri', 'hind-vadodara', 'holtwood-one-sc', 'homemade-apple', 'homenaje', 'im-fell-dw-pica', 'im-fell-dw-pica-sc', 'im-fell-double-pica', 'im-fell-double-pica-sc', 'im-fell-english', 'im-fell-english-sc', 'im-fell-french-canon', 'im-fell-french-canon-sc',
-    'im-fell-great-primer', 'im-fell-great-primer-sc', 'iceberg', 'iceland', 'imprima', 'inconsolata', 'inder', 'indie-flower', 'inika', 'inknut-antiqua', 'irish-grover', 'istok-web', 'italiana', 'italianno', 'itim', 'jacques-francois', 'jacques-francois-shadow',
-    'jaldi', 'jim-nightshade', 'jockey-one', 'jolly-lodger', 'jomhuria', 'josefin-sans', 'josefin-slab', 'joti-one', 'judson', 'julee', 'julius-sans-one', 'junge', 'jura', 'just-another-hand', 'just-me-again-down-here', 'kadwa', 'kalam', 'kameron', 'kanit',
-    'kantumruy', 'karla', 'karma', 'katibeh', 'kaushan-script', 'kavivanar', 'kavoon', 'kdam-thmor', 'keania-one', 'kelly-slab', 'kenia', 'khand', 'khmer', 'khula', 'kite-one', 'knewave', 'kotta-one', 'koulen', 'kranky', 'kreon', 'kristi', 'krona-one',
-    'kumar-one', 'kumar-one-outline', 'kurale', 'la-belle-aurore', 'laila', 'lakki-reddy', 'lalezar', 'lancelot', 'lateef', 'lato', 'league-script', 'leckerli-one', 'ledger', 'lekton', 'lemon', 'lemonada', 'libre-baskerville', 'libre-franklin', 'life-savers',
-    'lilita-one', 'lily-script-one', 'limelight', 'linden-hill', 'lobster', 'lobster-two', 'londrina-outline', 'londrina-shadow', 'londrina-sketch', 'londrina-solid', 'lora', 'love-ya-like-a-sister', 'loved-by-the-king', 'lovers-quarrel', 'luckiest-guy',
-    'lusitana', 'lustria', 'macondo', 'macondo-swash-caps', 'mada', 'magra', 'maiden-orange', 'maitree', 'mako', 'mallanna', 'mandali', 'marcellus', 'marcellus-sc', 'marck-script', 'margarine', 'marko-one', 'marmelad', 'martel', 'martel-sans', 'marvel',
-    'mate', 'mate-sc', 'maven-pro', 'mclaren', 'meddon', 'medievalsharp', 'medula-one', 'meera-inimai', 'megrim', 'meie-script', 'merienda', 'merienda-one', 'merriweather', 'merriweather-sans', 'metal', 'metal-mania', 'metamorphous', 'metrophobic', 'michroma',
-    'milonga', 'miltonian', 'miltonian-tattoo', 'miniver', 'miriam-libre', 'mirza', 'miss-fajardose', 'mitr', 'modak', 'modern-antiqua', 'mogra', 'molengo', 'molle', 'monda', 'monofett', 'monoton', 'monsieur-la-doulaise', 'montaga', 'montez', 'montserrat',
-    'montserrat-alternates', 'mon-tserrat-subrayada', 'moul', 'moulpali', 'mountains-of-christmas', 'mouse-memoirs', 'mr-bedfort', 'mr-dafoe', 'mr-de-haviland', 'mrs-saint-delafield', 'mrs-sheppards', 'mukta-vaani', 'muli', 'mystery-quest', 'ntr', 'neucha',
-    'neuton', 'new-rocker', 'news-cycle', 'niconne', 'nixie-one', 'nobile', 'nokora', 'norican', 'nosifer', 'nothing-you-could-do', 'noticia-text', 'noto-sans', 'noto-serif', 'nova-cut', 'nova-flat', 'nova-mono', 'nova-oval', 'nova-round', 'nova-script',
-    'nova-slim', 'nova-square', 'numans', 'nunito', 'nunito-sans', 'odor-mean-chey', 'offside', 'old-standard-tt', 'oldenburg', 'oleo-script', 'oleo-script-swash-caps', 'open-sans', 'open-sans-condensed', 'oranienbaum', 'orbitron', 'oregano', 'orienta',
-    'original-surfer', 'oswald', 'over-the-rainbow', 'overlock', 'overlock-sc', 'overpass', 'overpass-mono', 'ovo', 'oxygen', 'oxygen-mono', 'pt-mono', 'pt-sans', 'pt-sans-caption', 'pt-sans-narrow', 'pt-serif', 'pt-serif-caption', 'pacifico', 'padauk',
-    'palanquin', 'palanquin-dark', 'pangolin', 'paprika', 'parisienne', 'passero-one', 'passion-one', 'pathway-gothic-one', 'patrick-hand', 'patrick-hand-sc', 'pattaya', 'patua-one', 'pavanam', 'paytone-one', 'peddana', 'peralta', 'permanent-marker', 'petit-formal-script',
-    'petrona', 'philosopher', 'piedra', 'pinyon-script', 'pirata-one', 'plaster', 'play', 'playball', 'playfair-display', 'playfair-display-sc', 'podkova', 'poiret-one', 'poller-one', 'poly', 'pompiere', 'pontano-sans', 'poppins', 'port-lligat-sans', 'port-lligat-slab',
-    'pragati-narrow', 'prata', 'preahvihear', 'press-start-2p', 'pridi', 'princess-sofia', 'prociono', 'prompt', 'prosto-one', 'proza-libre', 'puritan', 'purple-purse', 'quando', 'quantico', 'quattrocento', 'quattrocento-sans', 'questrial', 'quicksand',
-    'quintessential', 'qwigley', 'racing-sans-one', 'radley', 'rajdhani', 'rakkas', 'raleway', 'raleway-dots', 'ramabhadra', 'ramaraja', 'rambla', 'rammetto-one', 'ranchers', 'rancho', 'ranga', 'rasa', 'rationale', 'ravi-prakash', 'redressed', 'reem-kufi',
-    'reenie-beanie', 'revalia', 'rhodium-libre', 'ribeye', 'ribeye-marrow', 'righteous', 'risque', 'roboto', 'roboto-condensed', 'roboto-mono', 'roboto-slab', 'rochester', 'rock-salt', 'rokkitt', 'romanesco', 'ropa-sans', 'rosario', 'rosarivo', 'rouge-script',
-    'rozha-one', 'rubik', 'rubik-mono-one', 'ruda', 'rufina', 'ruge-boogie', 'ruluko', 'rum-raisin', 'ruslan-display', 'russo-one', 'ruthie', 'rye', 'sacramento', 'sahitya', 'sail', 'salsa', 'sanchez', 'sancreek', 'sansita', 'sarala', 'sarina', 'sarpanch',
-    'satisfy', 'scada', 'scheherazade', 'schoolbell', 'scope-one', 'seaweed-script', 'secular-one', 'sevillana', 'seymour-one', 'shadows-into-light', 'shadows-into-light-two', 'shanti', 'share', 'share-tech', 'share-tech-mono', 'shojumaru', 'short-stack',
-    'shrikhand', 'siemreap', 'sigmar-one', 'signika', 'signika-negative', 'simonetta', 'sintony', 'sirin-stencil', 'six-caps', 'skranji', 'slabo-13px', 'slabo-27px', 'slackey', 'smokum', 'smythe', 'sniglet', 'snippet', 'snowburst-one', 'sofadi-one', 'sofia',
-    'sonsie-one', 'sorts-mill-goudy', 'source-code-pro', 'source-sans-pro', 'source-serif-pro', 'space-mono', 'special-elite', 'spicy-rice', 'spinnaker', 'spirax', 'squada-one', 'sree-krushnadevaraya', 'sriracha', 'stalemate', 'stalinist-one', 'stardos-stencil',
-    'stint-ultra-condensed', 'stint-ultra-expanded', 'stoke', 'strait', 'sue-ellen-francisco', 'suez-one', 'sumana', 'sunshiney', 'supermercado-one', 'sura', 'suranna', 'suravaram', 'suwannaphum', 'swanky-and-moo-moo', 'syncopate', 'tangerine', 'taprom',
-    'tauri', 'taviraj', 'teko', 'telex', 'tenali-ramakrishna', 'tenor-sans', 'text-me-one', 'the-girl-next-door', 'tienne', 'tillana', 'timmana', 'tinos', 'titan-one', 'titillium-web', 'trade-winds', 'trirong', 'trocchi', 'trochut', 'trykker', 'tulpen-one',
-    'ubuntu', 'ubuntu-condensed', 'ubuntu-mono', 'ultra', 'uncial-antiqua', 'underdog', 'unica-one', 'unifrakturcook', 'unifrakturmaguntia', 'unkempt', 'unlock', 'unna', 'vt323', 'vampiro-one', 'varela', 'varela-round', 'vast-shadow', 'vesper-libre', 'vibur',
-    'vidaloka', 'viga', 'voces', 'volkhov', 'vollkorn', 'voltaire', 'waiting-for-the-sunrise', 'wallpoet', 'walter-turncoat', 'warnes', 'wellfleet', 'wendy-one', 'wire-one', 'work-sans', 'yanone-kaffeesatz', 'yantramanav', 'yatra-one', 'yellowtail', 'yeseva-one',
-    'yesteryear', 'yrsa', 'zeyada'];
-
-
-fonts.forEach(font => {
-    let css = '';
-    css +=
-        `.u-font-${font} {
-         font-family: ${fontmap[font]} !important;
-    }
-    \n`;
-
-    weights.forEach(w => {
-css +=
-`.u-font-${font}-fw${w} {
-font-family: ${fontmap[font]} !important;
-font-weight: ${w} !important;
-}
-\n`;
-        sizes.forEach(s => {
-css +=
-`.u-font-${font}-fw${w}-fs${s} {
-font-family: ${fontmap[font]} !important;
-font-size: ${sizeMap[s]} !important;
-font-weight: ${w} !important;
-}
-\n`;
-        });
-    });
-    const subdir = `/${font}`;
-    let destdir = path.join(PLI.target.main.css, subdir);
-    let file = path.join(destdir, '/index.css');
-    mkdirp.sync(destdir);
-    fs.writeFileSync(file, css);
-
-    destdir = path.join(PLI.DIST, subdir);
-    file = path.join(destdir, '/index.css');
-    mkdirp.sync(destdir);
-    fs.writeFileSync(file, css);
-
+//================================
+//PARSE Google Fonts JSON
+//Obtained from the Google Fonts API
+//See:
+//https://fireflysemantics.medium.com/obtaining-a-json-document-with-all-google-fonts-9e9ea5cbd80
+//================================
+const DOCUMENT = JSON.parse(fs.readFileSync("./src/main/json/google-fonts.json", "utf-8"));
+const ITEMS = DOCUMENT.items;
+ITEMS.forEach((font) => {
+  defineFontUtility(font);
 });
+
+function defineFontUtility(font) {
+  //================================
+  //Array containing
+  //each variant instance.
+  //================================
+  const utilities = [];
+
+  //================================
+  //Dasherize the font name
+  //================================
+  const dashedFontName = font.family.replace(/\s+/g, "-").toLowerCase();
+
+  //================================
+  //Iterate over the variants
+  //of the font
+  //================================
+  font.variants.forEach((variant) => {
+    variant = variant.split(/(\d+)/);
+
+    //================================
+    //Dasherize the variant name
+    //For example 700italic >> 700-italic
+    //================================
+    if (variant.length) {
+      variant = variant.filter(Boolean).sort().join("-");
+    }
+
+    //================================
+    //Normalize all variant names
+    //For italic and normal the 400
+    //is implied.
+    //================================
+    variant = variant == "italic" ? "400-italic" : variant;
+    variant = variant.replace("regular", 400);
+    const class_name = `.u-font-${dashedFontName}-${variant}`;
+
+    if (class_name.includes("italic")) {
+      const utility = createItalicUtility(
+        class_name,
+        font.family,
+        variant.substring(0, 3)
+      );
+      utilities.push(utility);
+    } else {
+      const utility = createNormalUtility(
+        class_name,
+        font.family,
+        variant.substring(0, 3)
+      );
+      utilities.push(utility);
+    }
+    const fontCSS = utilities.join("");
+
+    saveFont(dashedFontName, fontCSS);
+  });
+}
+
+/**
+ * Will delete `index.css` before saving it again.
+ * @param {*} font The name of the font
+ * @param {*} css The css string to be saved
+ */
+function saveFont(font, css) {
+  const subdir = `/google/${font}`;
+  let destdir = path.join(PLI.src.main.css, subdir);
+  let file = path.join(destdir, "/index.css");
+  del.sync(file);
+  mkdirp.sync(destdir);
+  fs.writeFileSync(file, css);
+}
+
+function createNormalUtility(className, fontFamily, weight) {
+  return `${className} {
+          font-family: '${fontFamily}' !important;
+          font-weight: ${weight} !important;
+}\n`;
+}
+
+function createItalicUtility(className, fontFamily, weight) {
+  return `${className} {
+        font-family: '${fontFamily}' !important;
+        font-weight: ${weight} !important;
+        font-style: italic !important;
+}\n`;
+}
